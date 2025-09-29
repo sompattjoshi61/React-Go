7e964ca148e09801d9d71f21a60cc5dc7fe7eb9c
Som Pattjoshi <sompattjoshi61@gmail.com>
2025-09-29 22:54:58 +0530

    Drived-State

--- hooks-applications/src/App.jsx
+++ hooks-applications/src/App.jsx
@@ -62,36 +62,64 @@ function App() {
 
   
   // useFormStatus 
-  const [pending, setPending] = useState(false);
+  // const [pending, setPending] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault(); // prevent page reload
-    setPending(true);   // start loading
-    await new Promise((res) => setTimeout(res, 2000)); // simulate delay
-    console.log("Submitted");
-    setPending(false);  // stop loading
-  };
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault(); // prevent page reload
+  //   setPending(true);   // start loading
+  //   await new Promise((res) => setTimeout(res, 2000)); // simulate delay
+  //   console.log("Submitted");
+  //   setPending(false);  // stop loading
+  // };
 
-  function CustomerForm() {
-    return (
-      <div>
-        <input type="text" placeholder="Enter your name" />
-        <br /><br />
-        <input type="password" placeholder="Enter your password" />
-        <br /><br />
-        <button disabled={pending} type="submit">
-          {pending ? "Submitting..." : "Submit"}
-        </button>
-      </div>
-    );
+  // function CustomerForm() {
+  //   return (
+  //     <div>
+  //       <input type="text" placeholder="Enter your name" />
+  //       <br /><br />
+  //       <input type="password" placeholder="Enter your password" />
+  //       <br /><br />
+  //       <button disabled={pending} type="submit">
+  //         {pending ? "Submitting..." : "Submit"}
+  //       </button>
+  //     </div>
+  //   );
+  // }
+
+  // return (
+  //   <div>
+  //     <h1>useFormStatus Alternative in React</h1>
+  //     <form onSubmit={handleSubmit}>
+  //       <CustomerForm />
+  //     </form>
+  //   </div>
+  // )
+
+
+
+  //Drived State
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState([]);
+  const handleAddUsers = () => {
+    setUsers([...users,user])
   }
-
+  console.log(users)
   return (
     <div>
-      <h1>useFormStatus Alternative in React</h1>
-      <form onSubmit={handleSubmit}>
-        <CustomerForm />
-      </form>
+      <h2>Total Users: {users.length}</h2>
+      <h2>Last User: {user}</h2>
+      <h2>Unique Users: {new Set(users).size}</h2>
+      <h1>Drived State</h1>
+      <input type='text' onChange={(event) => {setUser(event.target.value)}} placeholder='Add new user'></input>
+      <button onClick={handleAddUsers}>Add User</button>
+      {
+        users.map((item,index) => (
+          <h4 key={index}>{item}</h4>
+        ))
+      }
+
+      
+
     </div>
   )
 
